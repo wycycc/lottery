c8665370e4c260bb3a4bab5febf61b0ca1953cf7
ycc <ycc2010@yeah.net>
2016-12-08 12:02:44 +0800

    更改项目图标

--- app/src/androidTest/java/com/ycc/lottery/ApplicationTest.java
+++ app/src/androidTest/java/com/ycc/lottery/ApplicationTest.java
@@ -2,6 +2,11 @@ package com.ycc.lottery;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlSerializer;
+
+import java.io.StringWriter;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -9,5 +14,27 @@ import android.test.ApplicationTestCase;
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    public void createXml() throws Exception {
+        //序列化
+        XmlSerializer serializer = Xml.newSerializer();
+        StringWriter writer = new StringWriter();
+        serializer.setOutput(writer);
+        serializer.startDocument(ConstantValue.ENCODING,null);
+
+        serializer.startTag(null,"message");
+        serializer.startTag(null,"header");
+
+        serializer.startTag(null,"agenterid");
+        serializer.text(ConstantValue.AGENTERID);
+        serializer.endTag(null,"agenterid");
+
+        serializer.endTag(null,"header");
+        serializer.startTag(null,"body");
+        serializer.endTag(null,"body");
+
+        serializer.endTag(null,"message");
+        serializer.endDocument();
     }
 }
---
+++ app/src/main/java/com/ycc/lottery/ConstantValue.java
@@ -0,0 +1,12 @@
+package com.ycc.lottery;
+
+/**
+ * Created by Administrator on 2016/12/8.
+ */
+public interface ConstantValue {
+    String ENCODING = "UTF-8";
+    /**
+     * 代理的id
+     */
+    String AGENTERID = "889931";
+}
